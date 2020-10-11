a0ebfa28ec540a77f9131c1074b9c014cb0767be
XVRomanova <XVRomanova@gmail.com>
2020-10-11 06:20:58 +0300

    fix pr issue

--- task1_linear_search/src/com/company/Main.java
+++ task1_linear_search/src/com/company/Main.java
@@ -27,6 +27,7 @@ public class Main {
         for (int i = 0; i < list.size(); ++i) {
             if (list.get(i) == number) {
                 index = i;
+                break;
             }
         }
 
