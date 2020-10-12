8093a52c99d2868b6d9a51ae6fedf8a2ad166793
XVRomanova <XVRomanova@gmail.com>
2020-10-12 21:08:09 +0300

    Changed code for testing: Added interface UnaryOperator for passing of  function as a parameter

--- task4_testing_code/src/com/company/Main.java
+++ task4_testing_code/src/com/company/Main.java
@@ -1,24 +1,24 @@
 package com.company;
 
+import java.util.function.UnaryOperator;
 
 public class Main {
 
     public static void main(String[] args) {
+
+    }
+
+    public static boolean test(int[] sortedList, int[] output) {
+        return Arrays.equals(sortedList, output);
+    }
+
+    static void allTest(UnaryOperator<int []> sort) {
         int[] input1 = {-1, 2, -5, 6, 7, 8};
         int[] input2 = {1, 1, 1};
         int[] input3 = {-5, -2, -1, -4, -6, -3};
         int[] input4 = {};
         int[] input5 = {4, 2, 3, 5};
         int[] input6 = {2, 1, 3, 1, 3};
-
-
-    }
-
-    public static boolean test(int[] sortedList, int[] output) {
-        return sortedList == output;
-    }
-
-    static void allTest(int[] sortedList) {
 
         int[] output1 = {-5, -1, 2, 6, 7, 8};
         int[] output2 = {1, 1, 1};
@@ -28,17 +28,17 @@ public class Main {
         int[] output6 = {1, 1, 2, 3, 3};
 
         System.out.print("Test 1: ");
-        System.out.println(test(sortedList, output1));
+        System.out.println(test(sort.apply(input1), output1));
         System.out.print("Test 2: ");
-        System.out.println(test(sortedList, output2));
+        System.out.println(test(sort.apply(input2), output2));
         System.out.print("Test 3: ");
-        System.out.println(test(sortedList, output3));
+        System.out.println(test(sort.apply(input3), output3));
         System.out.print("Test 4: ");
-        System.out.println(test(sortedList, output4));
+        System.out.println(test(sort.apply(input4), output4));
         System.out.print("Test 5: ");
-        System.out.println(test(sortedList, output5));
+        System.out.println(test(sort.apply(input5), output5));
         System.out.print("Test 6: ");
-        System.out.println(test(sortedList, output6));
+        System.out.println(test(sort.apply(input6), output6));
     }
 
 }
